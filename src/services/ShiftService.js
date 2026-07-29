bbb54aaf68289633a0ccd4f9b72e9adb859714a1
wang <quang2005n@gmail.com>
2026-07-29 07:15:55 +0700

    feat(shift): add ShiftService.js

--- src/services/ShiftService.js
+++ src/services/ShiftService.js
@@ -1,7 +1,16 @@
-import CommonService from './CommonService'
+import CommonService from '@/services/CommonService'
 
-const ShiftService = {
-  // TODO: implement Shift API calls
+export default {
+  listShifts(params = '') {
+    return $api(`shift?${params}`)
+  },
+  getShift(id) {
+    return $api(`shift/${id}`)
+  },
+  openShift(data) {
+    return $api('shift', { method: 'POST', body: CommonService.formData(data), loading: true })
+  },
+  closeShift(id, data) {
+    return $api(`shift/${id}/close`, { method: 'POST', body: CommonService.formData(data), loading: true })
+  },
 }
-
-export default ShiftService
