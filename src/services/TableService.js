00b984a5fd2699855e1853f71c30859767526dd9
wang <quang2005n@gmail.com>
2026-07-29 07:41:11 +0700

    feat(table): add TableService.js

--- src/services/TableService.js
+++ src/services/TableService.js
@@ -1,7 +1,22 @@
-import CommonService from './CommonService'
+import CommonService from '@/services/CommonService'
 
-const TableService = {
-  // TODO: implement Table API calls
+export default {
+  listTables(params = '') {
+    return $api(`table?${params}`)
+  },
+  getTable(id) {
+    return $api(`table/${id}`)
+  },
+  createTable(data) {
+    return $api('table', { method: 'POST', body: CommonService.formData(data), loading: true })
+  },
+  updateTable(data) {
+    return $api(`table/${data.id}`, { method: 'PUT', body: CommonService.formData(data), loading: true })
+  },
+  deleteTable(id) {
+    return $api(`table/${id}`, { method: 'DELETE', loading: true })
+  },
+  setStatus(id, data) {
+    return $api(`table/${id}/status`, { method: 'POST', body: CommonService.formData(data), loading: true })
+  },
 }
-
-export default TableService
