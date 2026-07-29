b5ea34659ea27acade3a6f4dc0690ddae6fc8972
wang <quang2005n@gmail.com>
2026-07-29 11:36:54 +0700

    feat(loyalty): add LoyaltyService.js

--- src/services/LoyaltyService.js
+++ src/services/LoyaltyService.js
@@ -1,7 +1,22 @@
-import CommonService from './CommonService'
+import CommonService from '@/services/CommonService'
 
-const LoyaltyService = {
-  // TODO: implement Loyalty API calls
+export default {
+  listCustomers(params = '') {
+    return $api(`loyalty/customer?${params}`)
+  },
+  getCustomer(id) {
+    return $api(`loyalty/customer/${id}`)
+  },
+  createCustomer(data) {
+    return $api('loyalty/customer', { method: 'POST', body: CommonService.formData(data), loading: true })
+  },
+  updateCustomer(data) {
+    return $api(`loyalty/customer/${data.id}`, { method: 'PUT', body: CommonService.formData(data), loading: true })
+  },
+  earn(id, data) {
+    return $api(`loyalty/customer/${id}/earn`, { method: 'POST', body: CommonService.formData(data), loading: true })
+  },
+  redeem(id, data) {
+    return $api(`loyalty/customer/${id}/redeem`, { method: 'POST', body: CommonService.formData(data), loading: true })
+  },
 }
-
-export default LoyaltyService
